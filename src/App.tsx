0f82c92ce27c3df335bc75810ce72721ab4659bd
Olivia Rodrigues <olivia.rt@gmail.com>
2022-06-04 01:25:43 -0300

    usa o contexto do usuario no App
    com esse contexto, teremos o usuario logado em qualquer parte da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { GlobalStyles, Theme } from "./assets/styles/Global";
 import { Routes, Route } from "react-router-dom";
 
@@ -12,20 +12,38 @@ import { Receitas } from "./pages/Receitas";
 import { Despesas } from "./pages/Despesas";
 import { DespesasCompartilhadas } from "./pages/DespesasCompartilhadas";
 
+import { UserContext } from "./usuario";
+import { Usuario } from "./@types/api";
+import { getUsuario } from "./api/usuario";
+
 export function App() {
+  const [user, setUser] = React.useState<Usuario | undefined>();
+
+  // temporariamente setando usuario 1 como padrao
+  useEffect(() => {
+    getUsuario(1).then((usuario) => {
+      setUser(usuario);
+    });
+  }, []);
+
   return (
     <>
       <Global styles={GlobalStyles} />
       <ThemeProvider theme={Theme}>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/cadastro' element={<Cadastro />} />
-          <Route path='/dashboard' element={<Dashboard />} />
-          <Route path='/receitas' element={<Receitas />} />
-          <Route path='/despesas' element={<Despesas />} />
-          <Route path='/despesas-compartilhadas' element={<DespesasCompartilhadas />} />
-        </Routes>
+        <UserContext.Provider value={user}>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/cadastro' element={<Cadastro />} />
+            <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/receitas' element={<Receitas />} />
+            <Route path='/despesas' element={<Despesas />} />
+            <Route
+              path='/despesas-compartilhadas'
+              element={<DespesasCompartilhadas />}
+            />
+          </Routes>
+        </UserContext.Provider>
       </ThemeProvider>
     </>
   );

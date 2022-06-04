1e1080cf0ad2946ab93af6475c2b26c71e10d542
Olivia Rodrigues <olivia.rt@gmail.com>
2022-06-04 01:58:59 -0300

    Pagina cadastro em alta fidelidade

--- src/pages/Cadastro/index.tsx
+++ src/pages/Cadastro/index.tsx
@@ -1,38 +1,59 @@
+import ImagemLogin from "../../assets/images/Cadastro/cadastro.svg";
+import Logo from "../../assets/images/logo.svg";
+
 import { Button, Divider, TextField, Typography } from "@mui/material";
 import { FaGoogle } from "react-icons/fa";
+import { useMediaQuery } from "react-responsive";
 import { Link } from "react-router-dom";
 import { Header } from "../../components/Header";
-import { Container, FormContainer, LoginGoogle } from "./Cadastro.style";
+import {
+  Container,
+  ContainerForm,
+  ContainerImagem,
+  FormContainer,
+  LoginGoogle,
+} from "./Cadastro.style";
 
 export const Cadastro = () => {
+  const desktop = useMediaQuery({
+    query: "(min-width:1025px)",
+  });
+
   return (
     <Container>
-      <Header HiddenBotao={true} />
-      <Typography variant='h3'>Criar conta</Typography>
-      <LoginGoogle>
-        <Button variant='borderPrimary' fullWidth startIcon={<FaGoogle />}>
-          Entrar com o Google
-        </Button>
-      </LoginGoogle>
-      <Divider>ou</Divider>
-      <FormContainer>
-        <TextField variant='outlined' label='Digite seu nome'></TextField>
-        <TextField variant='outlined' label='Digite seu email' type='email'></TextField>
-        <TextField variant='outlined' label='Digite sua senha' type='password'></TextField>
-        <TextField variant='outlined' label='Repita sua senha' type='password'></TextField>
-        <Link to='/login'>
-          <Button variant='borderPrimary' fullWidth>
-            Cadastrar
+      <ContainerForm>
+        <Link to='/'>
+          <img className='logo' src={Logo} alt='' />
+        </Link>
+        <Typography variant='h3' component='h1' color='complementaryBlack' sx={{ fontWeight: 700 }}>
+          Criar conta
+        </Typography>
+        <LoginGoogle>
+          <Button variant='borderGoogle' fullWidth startIcon={<FaGoogle />}>
+            Registrar com o Google
           </Button>
-        </Link>
-      </FormContainer>
+        </LoginGoogle>
+        <Divider sx={{ width: "100%" }}>ou</Divider>
+        <FormContainer>
+          <TextField variant='outlined' label='Nome'></TextField>
+          <TextField variant='outlined' label='Email' type='email'></TextField>
+          <TextField variant='outlined' label='Senha' type='password'></TextField>
+          <TextField variant='outlined' label='Repita sua senha' type='password'></TextField>
+          <Link to='/login'>
+            <Button variant='borderSecondary' fullWidth>
+              Cadastrar
+            </Button>
+          </Link>
+        </FormContainer>
 
-      <Typography variant='body1' align='center'>
-        Já tenho uma conta?{" "}
-        <Link to='/login'>
-          <Typography variant='bold'>Acessar conta</Typography>
-        </Link>
-      </Typography>
+        <Typography variant='body1' align='center'>
+          Já tenho uma conta?{" "}
+          <Link to='/login'>
+            <Typography variant='bold'>Acessar conta</Typography>
+          </Link>
+        </Typography>
+      </ContainerForm>
+      {desktop && <ContainerImagem img={ImagemLogin} />}
     </Container>
   );
 };
